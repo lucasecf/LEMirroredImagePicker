b52a7677928a4caff50642e4f24a05db0517315b
Lucas Eduardo <lucasecf@gmail.com>
2015-09-24 13:54:47 -0300

    Updating projeto to ios9 and xcode7

---
+++ Example/Pods/Target Support Files/LEMirroredImagePicker/LEMirroredImagePicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LEMirroredImagePicker : NSObject
+@end
+@implementation PodsDummy_LEMirroredImagePicker
+@end
--- Example/Pods/Target Support Files/Pods-LEMirroredImagePicker-LEMirroredImagePicker/Pods-LEMirroredImagePicker-LEMirroredImagePicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LEMirroredImagePicker_LEMirroredImagePicker : NSObject
-@end
-@implementation PodsDummy_Pods_LEMirroredImagePicker_LEMirroredImagePicker
-@end
